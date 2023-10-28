a77a0bbeb4eb9801404144247073e8a8e08cc172
devBoden <bodenhardinger@gmail.com>
2023-10-27 18:00:15 -0600

    fixed login functionality

--- account.js
+++ account.js
@@ -1,3 +1,4 @@
-getPlayerName() {
-    return localStorage.getItem('userName') ?? 'Mystery player';
-  }
+let Name = localStorage.getItem("userName")
+console.log(Name)
+let Username = document.getElementById("Name")
+Username.innerText=Name
