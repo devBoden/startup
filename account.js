16ec7ebc256077643a8c0faa816f66d33927e712
devBoden <bodenhardinger@gmail.com>
2023-10-28 20:48:24 -0600

    added occupation bar and finished skills bar

--- account.js
+++ account.js
@@ -2,3 +2,13 @@ let Name = localStorage.getItem("userName")
 console.log(Name)
 let Username = document.getElementById("Name")
 Username.innerText=Name
+
+let skills = localStorage.getItem("skillsText")
+console.log(skills)
+let SkillsText = document.getElementById("skills")
+SkillsText.innerText=skills
+
+let occupation = localStorage.getItem("occupationText")
+console.log(occupation)
+let OccupationText = document.getElementById("occupation")
+OccupationText.innerText=occupation
--- login.js
+++ login.js
@@ -2,4 +2,12 @@ function login() {
     const nameEl = document.querySelector("#name");
     localStorage.setItem("userName", nameEl.value);
     window.location.href = "account.html"
+
+    const skillsinputEl = document.querySelector("#skillsinput");
+    localStorage.setItem("skillsText", skillsinputEl.value);
+    window.location.href = "account.html"
+
+    const occupationInputEl = document.querySelector("#occupationInput");
+    localStorage.setItem("occupationText", occupationInputEl.value);
+    window.location.href = "account.html"
 }
